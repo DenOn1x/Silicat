d6769c5bc248d29be8db483d03a4928d5a3938c0
DenOn1x <den.onix2002@gmail.com>
2022-11-28 17:49:01 +0300

    .

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+
+let menuBtnPage = document.querySelector('.menu-btn-page');
+let menuBlockPage = document.querySelector('.block-menu');
+menuBtnPage.addEventListener('click', function(){
+    menuBtnPage.classList.toggle('active');
+    menuBlockPage.classList.toggle('active');
+})
