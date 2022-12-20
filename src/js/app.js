ba2890bfd63195e19cce1843232e75f82307b90f
DenOn1x <den.onix2002@gmail.com>
2022-12-20 17:39:34 +0300

    deleted comments

--- src/js/app.js
+++ src/js/app.js
@@ -66,7 +66,7 @@ ymaps.ready(function () {
         vetroparkWithContent = new ymaps.Placemark(coordVeropark, {
             hintContent: 'Собственный значок метки с контентом',
             balloonContent: 'А эта — новогодняя',
-            iconContent: '12'
+            iconContent: '1'
         }, {
             // Опции.
             // Необходимо указать данный тип макета.
@@ -86,7 +86,7 @@ ymaps.ready(function () {
     houseWithContent = new ymaps.Placemark(coordHouse, {
         hintContent: 'Собственный значок метки с контентом',
         balloonContent: 'А эта — новогодняя',
-        iconContent: '12'
+        iconContent: '2'
     }, {
         // Опции.
         // Необходимо указать данный тип макета.
@@ -106,7 +106,7 @@ ymaps.ready(function () {
         hotelWithContent = new ymaps.Placemark(coordHotel, {
             hintContent: 'Собственный значок метки с контентом',
             balloonContent: 'А эта — новогодняя',
-            iconContent: '12'
+            iconContent: '3'
         }, {
             // Опции.
             // Необходимо указать данный тип макета.
@@ -126,7 +126,7 @@ ymaps.ready(function () {
     silicatWithContent = new ymaps.Placemark(coordSilicat, {
         hintContent: 'Собственный значок метки с контентом',
         balloonContent: 'А эта — новогодняя',
-        iconContent: '12'
+        iconContent: '4'
     }, {
         // Опции.
         // Необходимо указать данный тип макета.
@@ -150,12 +150,12 @@ ymaps.ready(function () {
         .add(hotelWithContent)
         .add(silicatWithContent);
 
-    myMap.controls.remove('geolocationControl'); // удаляем геолокацию
-    myMap.controls.remove('searchControl'); // удаляем поиск
-    myMap.controls.remove('trafficControl'); // удаляем контроль трафика
-    myMap.controls.remove('typeSelector'); // удаляем тип
-    myMap.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-    myMap.controls.remove('zoomControl'); // удаляем контрол зуммирования
-    myMap.controls.remove('rulerControl'); // удаляем контрол правил
-    myMap.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+    myMap.controls.remove('geolocationControl');
+    myMap.controls.remove('searchControl');
+    myMap.controls.remove('trafficControl');
+    myMap.controls.remove('typeSelector');
+    myMap.controls.remove('fullscreenControl');
+    myMap.controls.remove('zoomControl');
+    myMap.controls.remove('rulerControl');
+    myMap.behaviors.disable(['scrollZoom']);
 });
